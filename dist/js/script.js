bb491af08bbfb95e312aa0cee3fd0a88fad07ea4
unknown <haryadib89@gmail.com>
2023-01-22 11:07:09 +0700

    terbaru

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,69 @@ window.onscroll = function () {
     header.classList.remove("navbar-fixed");
   }
 };
+
+// scroll to top
+function scrollToTop() {
+  const scrollTop = document.querySelector("#scroll-top");
+  if (window.pageYOffset > 500) {
+    scrollTop.classList.add("show-scroll-top");
+  } else {
+    scrollTop.classList.remove("show-scroll-top");
+  }
+}
+window.addEventListener("scroll", scrollToTop);
+
+let carouselSlider = document.querySelector("#carousel_slider");
+let list = document.querySelector("#carousel_list");
+let item = document.querySelectorAll("#carousel_item");
+let list2;
+
+const speed = 1;
+
+const width = list.offsetWidth;
+let x = 0;
+let x2 = width;
+
+function clone() {
+  list2 = list.cloneNode(true);
+  carouselSlider.appendChild(list2);
+  list2.style.left = `${width}px`;
+}
+
+function moveFirst() {
+  x -= speed;
+
+  if (width >= Math.abs(x)) {
+    list.style.left = `${x}px`;
+  } else {
+    x = width;
+  }
+}
+
+function moveSecond() {
+  x2 -= speed;
+
+  if (list2.offsetWidth >= Math.abs(x2)) {
+    list2.style.left = `${x2}px`;
+  } else {
+    x2 = width;
+  }
+}
+
+function hover() {
+  clearInterval(a);
+  clearInterval(b);
+}
+
+function unhover() {
+  a = setInterval(moveFirst, 10);
+  b = setInterval(moveSecond, 10);
+}
+
+clone();
+
+let a = setInterval(moveFirst, 10);
+let b = setInterval(moveSecond, 10);
+
+carouselSlider.addEventListener("mouseenter", hover);
+carouselSlider.addEventListener("mouseleave", unhover);
